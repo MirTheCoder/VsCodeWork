17ace0f79f685c3f20f0f86d606d34e7069c8890
MirTheCoder <mokoli142@gmail.com>
2025-12-30 22:32:27 -0500

    Initial start to library system, setting up basic framework and index page for the library system, more to come

---
+++ LibrarySystem/Server/server.js
@@ -0,0 +1,27 @@
+// Required libraries and modules that we need to install in our server file
+const express = require('express');
+const app = express();
+const path = require('path');
+const session = require('express-session');
+const MongoStore = require('connect-mongo').default;
+const {connect, getCollection, disconnect, getDb } = require('./middleware/db');
+const clientRoot = __dirname + '/../Client';
+let db = null;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(clientRoot, 'public')));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(clientRoot, 'indexPage.html'));
+    console.log(`Type: ${req.method} \n Url: ${req.url}`);
+})    
+
+
+
+
+//This will help us start our server along with connecting to the database
+app.listen(3000, async () => {
+    console.log('Server is running on http://localhost:3000');
+    //db = await connect();
+});
