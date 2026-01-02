3b79dd1f3173d7bff9c185dfa1df8edd6472d389
Miracle Okoli <mokoli142@gmal.com>
2026-01-01 21:05:46 -0500

    We have now indcluded some basic login and registration pages just to set up the functionality. Looking to also set up the otehr pages before entering into mongo db work and sessions

---
+++ LibrarySystem/Client/public/js/index.js
@@ -0,0 +1,15 @@
+let welcome1 = document.getElementById('w1');
+let welcome2 = document.getElementById('w2');
+let welcome3 = document.getElementById('w3');
+
+welcome1.addEventListener('click', () => {
+    alert("You choose to click to choose from a variety of books!");
+});
+
+welcome2.addEventListener('click', () => {
+    alert("You choose to click to see Reviews on your favorite books!");
+});
+
+welcome3.addEventListener('click', () => {
+    alert("You choose to click to donate a book (pdf only)!");
+});    
